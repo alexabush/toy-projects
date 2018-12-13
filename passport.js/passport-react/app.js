01dff9ac2503e60274270e32b03b1163ebf19777
Alex Bush <alexabush7@gmail.com>
2018-12-12 17:40:51 -0800

    added passport and session to app

--- passport.js/passport-react/app.js
+++ passport.js/passport-react/app.js
@@ -1,18 +1,23 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
+var passport = require('./config/passport');
 var index = require('./routes/index');
-
 var app = express();
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(
+  session({ secret: 'keyboard cat', resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', index);
 

5b0a1a8a85ff037e855ae0b27361028e4cee8faa
Alex Bush <alexabush7@gmail.com>
2018-12-26 12:35:55 -0800

    Made small project using context api

--- context-api/src/App.js
+++ context-api/src/App.js
@@ -1,28 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, createContext } from 'react';
 
+/* React.createContext is a method which is passed the initial value
+
+The object returned contains the Provider and Consumer JSX blocks
+*/
+const Context = createContext('instructor');
+let instructors = ['Matt', 'Elie', 'Joel', 'Michael']
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Hello From App! Let's pass some data down!</h1>
+        <Context.Provider value={instructors}>
+          <Grandparent />
+        </Context.Provider>
       </div>
     );
   }
+}
+
+const Grandparent = props => (
+  <div>
+    <h1>Grandparent</h1>
+    <Parent />
+  </div>
+);
+
+const Parent = props => (
+  <div>
+    <h1>Hello from Parent!</h1>
+    <Child />
+  </div>
+);
+
+const Child = props => (
+  <div>
+    <h1>Hello from Child!</h1>
+    <Context.Consumer>
+      {instructorName => {
+        return (
+        <div>
+          <h2>Here are the instructors: </h2>
+          <ul>
+              {getJSXList(instructorName)}
+          </ul>
+        </div>
+        )
+        }}
+    </Context.Consumer>
+  </div>
+);
+
+function getJSXList(list) {
+  return list.map(item => {
+    return <li>{item}</li>
+  })
 }
 
 export default App;
